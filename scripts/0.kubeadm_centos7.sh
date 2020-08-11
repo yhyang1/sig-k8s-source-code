ab54afdb7cb0c0f4d3c7c6e577d334cb86c0f186
杨萌 <meng@yangmengdeMac-mini.local>
2020-08-11 23:08:41 +0800

    add script for init kubeadm

---
+++ scripts/0.kubeadm_centos7.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+EOF
+setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+yum install -y kubelet kubeadm kubectl
+systemctl enable kubelet && systemctl start kubelet
